03d89fa2db0fa38fab636e7ffd3a261a976f6bf5
Ryan Farley <ryan@farleyzone.com>
2018-02-01 12:17:41 -0800

    Moved MailQueue entity rules to assembly. New deliverable.

---
+++ FX.MailQueue/Rules.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Sage.Entity.Interfaces;
+
+namespace FX.MailQueue
+{
+    public class Rules
+    {
+        public static void OnCreate(IMailQueue mailqueue)
+        {
+            mailqueue.IsHtml = true;
+        }
+
+        public static void MailQueueProcessedStep(IMailQueue mailqueue)
+        {
+        }
+    }
+}
